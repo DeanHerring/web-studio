8636ae86d34cfec230b56b1ce0be5b316d07a6db
0xGod <ddak1914@gmail.com>
2023-01-27 15:46:58 +0200

    Альфа релиз

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,21 @@
 // JS code...
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const dropdownOpen = document.querySelector(".dropdown-open");
+const dropdownOverlay = document.querySelector(".dropdown__menu-overlay");
+const dropdown = document.querySelector(".dropdown__menu");
+const dropdownList = document.querySelector(".dropdown__menu-list");
+
+dropdownOpen.addEventListener("click", () => {
+  dropdownList.classList.add("show");
+  dropdown.classList.add("show");
+});
+
+dropdownOverlay.addEventListener("click", () => {
+  dropdown.classList.remove("show");
+  dropdownList.classList.remove("show");
+});
--- docs/js/ol_slider.js
+++ docs/js/ol_slider.js
@@ -1,59 +1,62 @@
 // JS code...
-const slides = document.querySelectorAll('.ol__slide')
-const slidesWrapper = document.querySelector('.ol__wrapper')
+const slides = document.querySelectorAll(".ol__slide");
+const slidesWrapper = document.querySelector(".ol__wrapper");
 
-const dotsWrapper = document.querySelector('.ol__dots')
-const dot = document.querySelectorAll('.ol__dot')
+const dotsWrapper = document.querySelector(".ol__dots");
+const dot = document.querySelectorAll(".ol__dot");
 
-const images = document.querySelectorAll('.review__image')
+const images = document.querySelectorAll(".review__image");
 
-const slidesLength = slides.length
+const slidesLength = slides.length;
 
 // Добавление data-атрибутов
-slides.forEach((slide, index) => slide.setAttribute('data-slide', index + 1))
+slides.forEach((slide, index) => slide.setAttribute("data-slide", index + 1));
 
-images.forEach((image, index) => image.setAttribute('data-image', index + 1))
+images.forEach((image, index) => image.setAttribute("data-image", index + 1));
 
 // Вычисление общей длинны слайдера
-slidesWrapper.style.width = `${slidesLength * 100}%`
+slidesWrapper.style.width = `${slidesLength * 100}%`;
+// slides.forEach((slide) => (slide.style.width = `${100 / slidesLength}%`));
 
 // Вычисления количества страниц слайдера
-for ( let i = 1; i <= slidesLength; i++ ) {
-	const cloneDot = dot[0].cloneNode(true)
+for (let i = 1; i <= slidesLength; i++) {
+  const cloneDot = dot[0].cloneNode(true);
 
-	cloneDot.classList.remove('hidden')
-	cloneDot.setAttribute('data-id', i)
-	cloneDot.innerText = ""
+  cloneDot.classList.remove("hidden");
+  cloneDot.setAttribute("data-id", i);
+  cloneDot.innerText = "";
 
-	if ( cloneDot.getAttribute('data-id') == 1 ) {
-		cloneDot.classList.add('active')
-	}
+  if (cloneDot.getAttribute("data-id") == 1) {
+    cloneDot.classList.add("active");
+  }
 
-	dotsWrapper.appendChild(cloneDot)
+  dotsWrapper.appendChild(cloneDot);
 }
 
-const dots = [...dotsWrapper.children]
+const dots = [...dotsWrapper.children];
 
-dots.shift()
+dots.shift();
 
-dots.forEach(i => {
-	i.addEventListener('click', e => {
-		const dotId = i.getAttribute('data-id')
-		
-		const nextSlide = slides[dotId - 1]
-		const nextSlideId = nextSlide.getAttribute('data-slide')
+dots.forEach((i) => {
+  i.addEventListener("click", (e) => {
+    const dotId = i.getAttribute("data-id");
 
-		const activeSlide = [...slides].filter(slide => slide.classList.contains('active'))
-		const activeSlideId = activeSlide[0].getAttribute('data-slide')
+    const nextSlide = slides[dotId - 1];
+    const nextSlideId = nextSlide.getAttribute("data-slide");
 
-		const nextImage = images[dotId - 1]
+    const activeSlide = [...slides].filter((slide) =>
+      slide.classList.contains("active")
+    );
+    const activeSlideId = activeSlide[0].getAttribute("data-slide");
 
-		dots.forEach(a => a.classList.remove("active"))
-		images.forEach(image => image.classList.remove('show'))
+    const nextImage = images[dotId - 1];
 
-		i.classList.add('active')
-		nextImage.classList.add('show')
-	
-		slidesWrapper.style.marginLeft = `-${(nextSlideId - 1) * 100}%`
-	})
-})
+    dots.forEach((a) => a.classList.remove("active"));
+    images.forEach((image) => image.classList.remove("show"));
+
+    i.classList.add("active");
+    nextImage.classList.add("show");
+
+    slidesWrapper.style.marginLeft = `-${(nextSlideId - 1) * 100}%`;
+  });
+});
